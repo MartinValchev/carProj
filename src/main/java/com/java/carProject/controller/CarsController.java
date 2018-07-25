c40621878381d18f20d1f6000ff8ffa57481a93f
MartinValchev <MartinValchev@users.noreply.github.com>
2018-07-25 18:36:03 +0300

    added functionality to display a car by it id

--- src/main/java/com/java/carProject/controller/CarsController.java
+++ src/main/java/com/java/carProject/controller/CarsController.java
@@ -2,14 +2,18 @@ package com.java.carProject.controller;
 
 import com.java.carProject.entity.Cars;
 import com.java.carProject.entity.Parts;
+import com.java.carProject.repository.CarsRepository;
 import com.java.carProject.service.CarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,12 +22,21 @@ public class CarsController {
 
     @Autowired
     CarsService carsService;
+    @Autowired
+    CarsRepository carsRepository;
+
 
     @GetMapping("/cars/{make}")
     public String getCarsByMake(@PathVariable String make, Model model){
         List<Cars> carsByMakeList = carsService.getCarsByMake(make);
         model.addAttribute("carsByMakeList",carsByMakeList);
         return "carsByMake";
+    }
+    @GetMapping("/cars/id/{id}")
+    public String getCarById(@PathVariable Long id, Model model){
+        Cars cars = carsService.getCarById(id);
+        model.addAttribute("cars",cars);
+        return "carById";
     }
 
     @GetMapping("/cars/{id}/parts")
@@ -46,5 +59,11 @@ public class CarsController {
         model.addAttribute("cars", new Cars());
         return "AddCars";
     }
+  /*  @PostMapping("/AddCars")
+    public RedirectView addCars(@ModelAttribute("cars") Cars cars){
+        Cars savedCars = carsRepository.save(cars);
+    }*/
+
+
 
 }
