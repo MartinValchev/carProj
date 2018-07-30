d54c856126d808ac4274f5b69045d3d61c96d4cf
martin.valchev <martin.valchev1@gmail.com>
2018-07-30 17:11:25 +0300

    Added input validation for add customer

--- src/main/java/com/java/carProject/controller/CustomerController.java
+++ src/main/java/com/java/carProject/controller/CustomerController.java
@@ -2,20 +2,27 @@ package com.java.carProject.controller;
 
 import com.java.carProject.entity.Cars;
 import com.java.carProject.entity.Customers;
+import com.java.carProject.model.CustomerBindingModel;
 import com.java.carProject.repository.CustomerRepository;
 import com.java.carProject.service.CarsService;
 import com.java.carProject.service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Period;
@@ -27,6 +34,15 @@ import static org.springframework.http.ResponseEntity.created;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder("user")
+    public void customizeBinding(WebDataBinder binder){
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormatter.setLenient(false);
+        binder.registerCustomEditor(Date.class, "birthDate",
+                new CustomDateEditor(dateFormatter, true));
+    }
+
     @Autowired
     CustomerService customerService;
 
@@ -48,32 +64,38 @@ public class CustomerController {
     }
 
     @PostMapping("/customers")
-    public RedirectView addCustomer(@ModelAttribute("customers") Customers customers){
-        String returnPage ="";
-        LocalDate today = LocalDate.now();
-        Instant instant = customers.getBirthDate().toInstant();
-        ZoneId  defaultZoneId= ZoneId.systemDefault();
-        LocalDate customerLocalDate = instant.atZone(defaultZoneId).toLocalDate();
-        Period period = Period.between(customerLocalDate, today);
-        if(period.getYears()>=18) {
-            customers.setYoungDriver(false);
-        }else{
-            customers.setYoungDriver(true);
+    public String addCustomer(@Valid @ModelAttribute CustomerBindingModel customerBindingModel, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "AddCustomer";
+        }else {
+            ModelMapper modelMapper = new ModelMapper();
+            Customers customers =  modelMapper.map(customerBindingModel, Customers.class);
+            String returnPage = "";
+            LocalDate today = LocalDate.now();
+            Instant instant = customers.getBirthDate().toInstant();
+            ZoneId defaultZoneId = ZoneId.systemDefault();
+            LocalDate customerLocalDate = instant.atZone(defaultZoneId).toLocalDate();
+            Period period = Period.between(customerLocalDate, today);
+            if (period.getYears() >= 18) {
+                customers.setYoungDriver(false);
+            } else {
+                customers.setYoungDriver(true);
+            }
+            Customers savedCustomer = customerRepository.save(customers);
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(savedCustomer.getId()).toUri();
+            if (ResponseEntity.created(location).build().getStatusCode().equals(HttpStatus.CREATED)) {
+                Long customerId = savedCustomer.getId();
+                returnPage = "redirect:/customers/" + customerId;
+            } else {
+                // error page redirect
+            }
+            return returnPage;
         }
-        Customers savedCustomer = customerRepository.save(customers);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(savedCustomer.getId()).toUri();
-       if(ResponseEntity.created(location).build().getStatusCode().equals(HttpStatus.CREATED)){
-            Long customerId = savedCustomer.getId();
-           returnPage ="/customers/" + customerId;
-       }else {
-            // error page redirect
-       }
-        return new RedirectView(returnPage);
+
     }
 
     @GetMapping("/customers")
-    public String addCustomer(Model model){
-        model.addAttribute("customers", new Customers());
+    public String addCustomer(CustomerBindingModel customerBindingModel){
         return "AddCustomer";
     }
     @GetMapping("/customers/{id}")
---
+++ src/main/java/com/java/carProject/model/CustomerBindingModel.java
@@ -0,0 +1,40 @@
+package com.java.carProject.model;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class CustomerBindingModel {
+
+    @NotNull(message = "Customer name cannot be blank")
+    @Pattern(regexp = "(\\w+\\s?)+",message = "Invalid customer name")
+    private String name;
+
+    @NotNull(message = "Customer birthDate cannot be blank")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+
+
+}
