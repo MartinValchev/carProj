48d85c6c1e1b9c8b4c0f1c3fdee6f32827e9f310
martin.valchev <martin.valchev1@gmail.com>
2018-07-30 15:16:23 +0300

    added validation in AddCars form

--- src/main/java/com/java/carProject/controller/CarsController.java
+++ src/main/java/com/java/carProject/controller/CarsController.java
@@ -2,18 +2,22 @@ package com.java.carProject.controller;
 
 import com.java.carProject.entity.Cars;
 import com.java.carProject.entity.Parts;
+import com.java.carProject.model.CarsBindingModel;
 import com.java.carProject.repository.CarsRepository;
 import com.java.carProject.service.CarsService;
 import com.java.carProject.service.PartsService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 import java.util.HashSet;
 import java.util.List;
@@ -61,17 +65,22 @@ public class CarsController {
     }
 
     @GetMapping("/addCars")
-    public String getAddCars(Model model){
-        model.addAttribute("cars", new Cars());
+    public String getAddCars(@ModelAttribute CarsBindingModel carsBindingModel){
         return "AddCars";
     }
     @PostMapping("/addCars")
-    public RedirectView addCars(@ModelAttribute("cars") Cars cars){
-        String partIds = cars.getPartIds();
-        List<Parts> parts = partsService.generateParts(partIds);
-        cars.setParts(parts);
-        Cars savedCars = carsRepository.save(cars);
-        return new RedirectView("/cars/id/" + savedCars.getId() );
+    public String addCars(@Valid @ModelAttribute CarsBindingModel carsBindingModel, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "AddCars";
+        }else {
+            ModelMapper modelMapper = new ModelMapper();
+            Cars cars = modelMapper.map(carsBindingModel, Cars.class);
+            String partIds = cars.getPartIds();
+            List<Parts> parts = partsService.generateParts(partIds);
+            cars.setParts(parts);
+            Cars savedCars = carsRepository.save(cars);
+            return "redirect:/cars/id/" + savedCars.getId();
+        }
 
     }
 
--- src/main/java/com/java/carProject/model/CarsBindingModel.java
+++ src/main/java/com/java/carProject/model/CarsBindingModel.java
@@ -1,14 +1,55 @@
 package com.java.carProject.model;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class CarsBindingModel {
 
     @NotBlank(message="Make should not be blank")
-    @Size(min)
+    @Size(min=2, max=20)
     private String make;
-    private String model;
-    private long travelledDistance;
 
+    @NotBlank(message="Make should not be blank")
+    @Size(min=2, max=20)
+    private String model;
+
+    @NotNull
+    private Long travelledDistance;
+
+    @Pattern(regexp = "^(\\s?\\d+)+\\s?$",message = "Invalid id input")
+    private String partIds;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Long getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public void setTravelledDistance(Long travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+
+    public String getPartIds() {
+        return partIds;
+    }
+
+    public void setPartIds(String partIds) {
+        this.partIds = partIds;
+    }
 }
